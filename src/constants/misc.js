80d16193afdaff3ef41c0ebd2aaf0e92fed90b79
calebtuttle <1calebtuttle@gmail.com>
2022-09-29 15:33:09 -0400

    Make zkIdVerifyEndpoint dependent on NODE_ENV

--- src/constants/misc.js
+++ src/constants/misc.js
@@ -1,5 +1,7 @@
-export const zkIdVerifyEndpoint = "https://zk.sciverse.id";
 export const preprocEndpoint = "https://preproc-zkp.s3.us-east-2.amazonaws.com";
-// export const zkIdVerifyEndpoint = "http://localhost:3000"; // For tests
+export const zkIdVerifyEndpoint =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://zk.sciverse.id";
 
 export const serverAddress = "0x8281316aC1D51c94f2DE77575301cEF615aDea84";
