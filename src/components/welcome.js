1207066834dd6a1def1cc80a292f7374ad92dd46
nanaknihal <nanaknihal@gmail.com>
2022-10-06 00:37:03 -0400

    welcome screen

---
+++ src/components/welcome.js
@@ -0,0 +1,7 @@
+const Welcome = ()=>(
+<>
+<h1>Welcome to Holonym Alpha</h1>
+<a href={`https://holonym.id/verify`} className="x-button secondary outline">Verify to mint a Holo</a>
+</>
+)
+export default Welcome;
